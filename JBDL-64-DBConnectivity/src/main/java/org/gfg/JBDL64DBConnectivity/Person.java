5c1461f55af109cc96fd3be08f6a940ef0a2de98
kajal <51613822+Kajuglotra@users.noreply.github.com>
2024-01-14 22:36:30 +0530

    db

---
+++ JBDL-64-DBConnectivity/src/main/java/org/gfg/JBDL64DBConnectivity/Person.java
@@ -0,0 +1,17 @@
+package org.gfg.JBDL64DBConnectivity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class Person {
+
+    private String name;
+
+    private int id;
+
+}
