942e913ddbc4a0c6863c908655090b3dcd5eaf98
kajal <51613822+Kajuglotra@users.noreply.github.com>
2024-01-21 17:13:37 +0530

    Delete JBDL-64-DBConnectivity directory

--- JBDL-64-DBConnectivity/src/main/java/org/gfg/JBDL64DBConnectivity/repositories/MyPersonRepository.java
+++
@@ -1,7 +0,0 @@
-package org.gfg.JBDL64DBConnectivity.repositories;
-
-import org.gfg.JBDL64DBConnectivity.model.MyPerson;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MyPersonRepository extends JpaRepository<MyPerson,Integer> {
-}
